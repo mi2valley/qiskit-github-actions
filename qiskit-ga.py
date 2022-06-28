80ce391dad1898ff868cfb561ef64c90a18b0be1
mi2valley <monocoin189@gmail.com>
2022-06-28 16:35:00 +0900

    add qiskit ga

---
+++ qiskit-ga.py
@@ -0,0 +1,59 @@
+import os
+from qiskit import QuantumCircuit, execute, ClassicalRegister, QuantumRegister
+from qiskit.visualization import *
+from qiskit.providers.ibmq import least_busy
+from qiskit.tools.monitor import job_monitor
+import matplotlib.pyplot as plt
+
+
+IBMQ_TOKEN = os.getenv("IBMQ_TOKEN")
+IBMQ.save_account(str(IBMQ_TOKEN))
+
+
+
+# アカウント情報をロードして、使える量子デバイスを確認します
+IBMQ.load_account()
+provider = IBMQ.get_provider(hub='ibm-q', group='open', project='main')
+provider.backends()
+
+        # 最もすいているバックエンドを選びます
+large_enough_devices = IBMQ.get_provider(hub='ibm-q', group='open', project='main').backends(
+    filters=lambda x: x.configuration().n_qubits > 4 and not x.configuration().simulator and x.configuration().quantum_volume > 16 ) # and not x.configuration().backend_name == "ibmq_manila" and not x.configuration().backend_name == "ibmq_bogota")
+print(large_enough_devices)
+real_backend = least_busy(large_enough_devices)
+
+print("ベストなバックエンドは " + real_backend.name())
+
+# build quantum circuit
+qreg_q = QuantumRegister(5, 'q')
+creg_c = ClassicalRegister(5, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
+
+circuit.rz(1.5707963267948966, qreg_q[0])
+circuit.sx(qreg_q[0])
+circuit.rz(1.5707963267948966, qreg_q[0])
+circuit.rz(1.5707963267948966, qreg_q[1])
+circuit.sx(qreg_q[1])
+circuit.rz(1.5707963267948966, qreg_q[1])
+circuit.rz(1.5707963267948966, qreg_q[2])
+circuit.sx(qreg_q[2])
+circuit.rz(1.5707963267948966, qreg_q[2])
+circuit.rz(1.5707963267948966, qreg_q[3])
+circuit.sx(qreg_q[3])
+circuit.rz(1.5707963267948966, qreg_q[3])
+circuit.rz(1.5707963267948966, qreg_q[4])
+circuit.sx(qreg_q[4])
+circuit.rz(1.5707963267948966, qreg_q[4])
+circuit.measure(qreg_q[0], creg_c[0])
+circuit.measure(qreg_q[1], creg_c[1])
+circuit.measure(qreg_q[2], creg_c[2])
+circuit.measure(qreg_q[3], creg_c[3])
+circuit.measure(qreg_q[4], creg_c[4])
+
+# execute
+# 上記のバックエンドで実行します
+job = execute(circuit, real_backend)
+job_id = job.job_id()
+job_status = 0 #queued
+print(job_monitor(job))
+
